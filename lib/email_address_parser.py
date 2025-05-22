5a948459de6444161cb7bd51223154d871dc42e3
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-21 22:12:19 -0400

    Completes lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def parse(self):
+        normalized = self.addresses.replace(',', ' ')
+        candidates = normalized.split()
+        email_pattern = re.compile(r'^[a-zA-Z][\w\.-]*@[a-zA-Z\d-]+\.[a-zA-Z]{2,}$')
+        valid_emails = {email for email in candidates if email_pattern.fullmatch(email)}
+        return sorted(valid_emails)
